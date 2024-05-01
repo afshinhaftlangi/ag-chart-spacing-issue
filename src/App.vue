25fe22960cc018da8724f2ce465579cd29c6ea65
Afshin Haftlangi <afshin.haftlangi@hypeinnovation.com>
2024-05-01 11:38:17 -0400

    Cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ag-charts-vue :options="options"> </ag-charts-vue>
+  <ag-charts-vue :options="options1"> </ag-charts-vue>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { AgChartsVue } from 'ag-charts-vue3';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AgChartsVue
+  },
+  setup(){
+    return{
+      options: {
+        // Data: Data to be displayed in the chart
+        data: [
+          {  avgTemp: 2.3 },
+          {  avgTemp: 6.3},
+          {  avgTemp: 16.2},
+          {  avgTemp: 22.8 },
+          {  avgTemp: 14.5 },
+          {  avgTemp: 8.9},
+        ],
+        // Series: Defines which chart type and data to use
+        series: [{ type: 'pie', angleKey: 'avgTemp', legendItemKey: 'avgTemp' }],
+        legend:{
+          position:'right',
+          spacing:10
+        }
+      },
+      options1: {
+        // Data: Data to be displayed in the chart
+        data: [
+          {  avgTemp: 2.3 },
+          {  avgTemp: 6.3},
+          {  avgTemp: 16.2},
+          {  avgTemp: 22.8 },
+          {  avgTemp: 14.5 },
+          {  avgTemp: 8.9},
+        ],
+        // Series: Defines which chart type and data to use
+        series: [{ type: 'pie', angleKey: 'avgTemp', legendItemKey: 'avgTemp' }],
+        legend:{
+          position:'bottom',
+          spacing:10
+        }
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
